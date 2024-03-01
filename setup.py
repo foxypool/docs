8661140ae928d0a218c2556219f6f81133b0cf65
Felix Brucker <felix@felixbrucker.com>
2024-03-01 17:37:29 +0700

    Use setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='foxypool-docs',
+    version='1.0.0',
+    url='https://foxypool.io',
+    license='GPLv3',
+    author='Felix Brucker',
+    author_email='contact@foxypool.io',
+    description='Documentation for Foxy related services and tools',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    python_requires='>=3.11, <4',
+    install_requires=[
+        "mkdocs==1.5.3",
+        "mkdocs-material==9.5.11",
+        "mkdocs-minify-plugin==0.8.0",
+        "mkdocs-redirects==1.2.1",
+    ],
+)
